1a1960a54e00408a18fcb6c700b346104ca92410
Danendra <levydanendra775@gmail.com>
2026-07-28 11:13:14 +0700

    refactor: hapus fitur kuliner, penginapan, produk, dan umkm

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,8 @@ use App\Http\Controllers\WishlistController;
 Route::get('/', [PublicController::class, 'home'])->name('home');
 Route::get('/wisata', [PublicController::class, 'wisata'])->name('public.wisata');
 Route::get('/wisata/{slug}', [PublicController::class, 'wisataDetail'])->name('public.wisata.detail');
-Route::get('/umkm', [PublicController::class, 'umkm'])->name('public.umkm');
-Route::get('/produk', [PublicController::class, 'produk'])->name('public.produk');
-Route::get('/produk/{id}', [PublicController::class, 'produkDetail'])->name('public.produk.detail');
 Route::get('/event', [PublicController::class, 'event'])->name('public.event');
-Route::get('/kuliner', [PublicController::class, 'kuliner'])->name('public.kuliner');
-Route::get('/kuliner/{id}', [PublicController::class, 'kulinerDetail'])->name('public.kuliner.detail');
-Route::get('/penginapan', [PublicController::class, 'penginapan'])->name('public.penginapan');
-Route::get('/penginapan/{id}', [PublicController::class, 'penginapanDetail'])->name('public.penginapan.detail');
+
 Route::get('/berita', [PublicController::class, 'berita'])->name('public.berita');
 Route::get('/berita/{id}', [PublicController::class, 'beritaDetail'])->name('public.berita.detail');
 Route::get('/tentang', [PublicController::class, 'tentang'])->name('public.tentang');
